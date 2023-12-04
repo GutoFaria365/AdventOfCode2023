a11b1622a64fcc9b672fb00a20c5ee15b6bb0892
GutoFaria365 <122914909+GutoFaria365@users.noreply.github.com>
2023-12-04 04:37:03 +0000

    some fringe case ruining it

--- src/main/java/org/example/Day3.java
+++ src/main/java/org/example/Day3.java
@@ -6,19 +6,21 @@ import java.util.Map;
 public class Day3 {
     public static void main(String[] args) {
         String input =
-                "467..114..\n" +
-                "...*......\n" +
-                "..35..633.\n" +
-                "......#...\n" +
-                "617*......\n" +
-                ".....+.58.\n" +
-                "..592.....\n" +
-                "......755.\n" +
-                "...$.*....\n" +
-                ".664.598.";
+                "467..114............\n" +
+                "...*...........3339.\n" +
+                "..35..633...........\n" +
+                "......#.............\n" +
+                "617*................\n" +
+                ".....+.58...........\n" +
+                "..592...............\n" +
+                "......755...........\n" +
+                "...$.*..............\n" +
+                ".664.598............";
         String[] lines = input.split("\n");
         Map<Integer, Character> positionMapBefore = new HashMap<>();
-        Map<Integer, Character> positionMapAfter;
+        Map<Integer, Character> positionMapAfter = new HashMap<>();
+        int total = 0;
+        boolean validNumber = false;
         for (int lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             String line = lines[lineIndex];
             char[] characters = line.toCharArray();
@@ -59,26 +61,28 @@ public class Day3 {
 
                 }
 
-                if (previousChar != '.') {
-                    if (!Character.isLetterOrDigit(previousChar)){
-                        currentWord.append("V");
-                    }
-                }
-                if (nextChar != '.') {
-                    if (!Character.isLetterOrDigit(nextChar)){
-                        currentWord.append("V");
-                    }
 
-                }
+                validNumber = isValidNumber(validNumber, characters, i, previousChar);
+                validNumber = isValidNumber(validNumber, characters, i, nextChar);
+
+                validNumber = isValidNumber(validNumber, characters, i, positionMapBefore.get(i-1));
+                validNumber = isValidNumber(validNumber, characters, i, positionMapBefore.get(i));
+                validNumber = isValidNumber(validNumber, characters, i, positionMapBefore.get(i+1));
+
+                validNumber = isValidNumber(validNumber, characters, i, positionMapAfter.get(i-1));
+                validNumber = isValidNumber(validNumber, characters, i, positionMapAfter.get(i));
+                validNumber = isValidNumber(validNumber, characters, i, positionMapAfter.get(i+1));
 
 
-            }
-
-            for (char currentChar : characters) {
-                if (Character.isDigit(currentChar)) {
-                    currentWord.append(currentChar);
+                if (Character.isDigit(characters[i])) {
+                    currentWord.append(characters[i]);
 
                 } else if (currentWord.length() > 0) {
+                    if (validNumber){
+                        validNumber = false;
+                        total += Integer.parseInt(currentWord.toString());
+                        currentWord.append("v");
+                    }
                     System.out.println("Word: " + currentWord);
                     currentWord.setLength(0);
                 }
@@ -90,8 +94,19 @@ public class Day3 {
             positionMapBefore.clear();
             positionMapBefore.putAll(positionMap);
             System.out.println("Position Map: " + positionMap);
+            System.out.println("total = " + total);
             System.out.println("-".repeat(50));
         }
 
+    }
+
+    private static boolean isValidNumber(boolean validNumber, char[] characters, int i, Character nextChar) {
+        if (nextChar == null){
+            return validNumber;
+        }
+        if ((characters[i] >= '0' && characters[i] <= '9') && (nextChar != '.') && (nextChar < '0' || nextChar > '9')) {
+            validNumber = true;
+        }
+        return validNumber;
     }
 }
